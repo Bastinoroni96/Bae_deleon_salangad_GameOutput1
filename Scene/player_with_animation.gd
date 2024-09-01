f0b18aa3391ca1159c7b007ee3fab957dbee4b8c
Jaewan Bae <antjw1999@gmail.com>
2024-09-01 18:46:52 +0800

    withPlayerAnimation

--- Scene/player_with_animation.gd
+++ Scene/player_with_animation.gd
@@ -1,11 +1,12 @@
-extends Sprite2D
+extends CharacterBody2D
 
 @onready var tile_map = $"../TileMapLayer"
-@onready var sprite_2d = $Sprite2D
+@onready var sprite_2d = $AnimatedSprite2D
 
 var is_moving = false
+var AnimDirection = ''
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_moving == false:
 		return
 		
@@ -14,22 +15,37 @@ func _physics_process(delta):
 		return
 	
 	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	
+	if AnimDirection == 'UP':
+		sprite_2d.flip_h = false
+		sprite_2d.play('up')
+	elif AnimDirection == 'DOWN':
+		sprite_2d.flip_h = false
+		sprite_2d.play('down')
+	elif AnimDirection == "LEFT":
+		sprite_2d.flip_h = false
+		sprite_2d.play('left')
+	elif AnimDirection == "RIGHT":
+		sprite_2d.flip_h = true
+		sprite_2d.play('right')
+func _process(_delta):
 	if is_moving:
 		return
-		
+	
 	if Input.is_action_pressed("up"):
 		move(Vector2.UP)
+		AnimDirection = 'UP'
 	elif Input.is_action_pressed("down"):
 		move(Vector2.DOWN)
+		AnimDirection = 'DOWN'
 	elif Input.is_action_pressed("left"):
 		move(Vector2.LEFT)
+		AnimDirection = 'LEFT'
 	elif Input.is_action_pressed("right"):
 		move(Vector2.RIGHT)
-	
-func move(direction: Vector2):
+		AnimDirection = 'RIGHT'
+		
+func move(direction:Vector2i):
 	# Get current tile Vector2i
 	var current_tile: Vector2i = tile_map.local_to_map(global_position)
 	# Get target tile Vector2i
@@ -38,13 +54,13 @@ func move(direction: Vector2):
 		current_tile.y + direction.y,
 	)
 	prints(current_tile, target_tile)
+	
 	# Get custom data layer from target tile
 	var tile_data: TileData = tile_map.get_cell_tile_data(target_tile)
 	
 	if tile_data == null or tile_data.get_custom_data("walkable") == false:
 		return
-
-	
+		
 	# Move player 
 	is_moving = true
 	
---
+++ sources.gd
@@ -0,0 +1,6 @@
+# the sources where I got the code
+
+
+
+#https://www.youtube.com/watch?v=9u1Dq6h7sGU
+#https://www.youtube.com/watch?v=LOhfqjmasi0&t=903s
