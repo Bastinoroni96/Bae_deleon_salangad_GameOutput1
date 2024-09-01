53800d9db33e7c04e1405c9a5086ecce0137008c
Jaewan Bae <antjw1999@gmail.com>
2024-09-01 20:29:45 +0800

    withIceTile

--- Scene/player_with_animation.gd
+++ Scene/player_with_animation.gd
@@ -16,6 +16,7 @@ func _physics_process(_delta):
 	
 	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
 	
+	# play with animation
 	if AnimDirection == 'UP':
 		sprite_2d.flip_h = false
 		sprite_2d.play('up')
@@ -53,18 +54,43 @@ func move(direction:Vector2i):
 		current_tile.x + direction.x,
 		current_tile.y + direction.y,
 	)
-	prints(current_tile, target_tile)
+	#prints(current_tile, target_tile)
 	
 	# Get custom data layer from target tile
 	var tile_data: TileData = tile_map.get_cell_tile_data(target_tile)
 	
 	if tile_data == null or tile_data.get_custom_data("walkable") == false:
 		return
+	#print(target_tile)
+	# forceMove the player
+	# note: ice tiles and arrow tiles are both walkable and slidable
+	if tile_data.get_custom_data("slidable") == true:
+		# get the 'next' tiles set and check if it's safe to walk (like stop when there's wall or no tile)
+		var temp_target_tile: Vector2i  = target_tile + Vector2i(direction.x,direction.y)
+		var next_tile_data: TileData = tile_map.get_cell_tile_data(temp_target_tile)
 		
+
+		while (next_tile_data != null) and (next_tile_data.get_custom_data("walkable") == true) and (next_tile_data.get_custom_data("slidable") == true):
+			temp_target_tile += Vector2i(direction.x,direction.y)
+			#current_tile = current_tile + Vector2i(direction.x, direction.y)
+			next_tile_data = tile_map.get_cell_tile_data(temp_target_tile)
+	
+		
+		# check if there's changes in the target_tile and temp_target_tile
+		if (temp_target_tile - Vector2i(direction.x,direction.y)) != target_tile:
+			target_tile = temp_target_tile- Vector2i(direction.x,direction.y)
+		
+		# resets the player in the starting point (try not to change the map cuz i hard core the position)
+		#target_tile = Vector2i(2,4)
+		#current_tile = Vector2i(2,4)
+		#global_position = tile_map.map_to_local(target_tile)
+		#sprite_2d.global_position = tile_map.map_to_local(current_tile)
+	
 	# Move player 
 	is_moving = true
 	
 	global_position = tile_map.map_to_local(target_tile)
-	
 	sprite_2d.global_position = tile_map.map_to_local(current_tile)
+	
+
 	
--- sources.gd
+++ sources.gd
@@ -4,3 +4,4 @@
 
 #https://www.youtube.com/watch?v=9u1Dq6h7sGU
 #https://www.youtube.com/watch?v=LOhfqjmasi0&t=903s
+#https://opengameart.org/content/ice-block
